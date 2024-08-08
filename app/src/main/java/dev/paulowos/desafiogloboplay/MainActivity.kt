4ed7e41fc15ef7036b87c80a40559f8835c7c583
Paulo Willian <paulowos@gmail.com>
2024-08-08 11:18:31 -0300

    feat: Dagger Hilt settings

--- app/src/main/java/dev/paulowos/desafiogloboplay/MainActivity.kt
+++ app/src/main/java/dev/paulowos/desafiogloboplay/MainActivity.kt
@@ -5,14 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.paulowos.desafiogloboplay.ui.theme.DesafioGloboplayTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +18,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             DesafioGloboplayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DesafioGloboplayTheme {
-        Greeting("Android")
     }
 }
