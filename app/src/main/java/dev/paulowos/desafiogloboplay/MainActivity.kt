64ba9ad688e9067dbf8dbd40747d982018c4fd26
Paulo Willian <paulowos@gmail.com>
2024-08-08 11:45:43 -0300

    feat: Navigation setting

--- app/src/main/java/dev/paulowos/desafiogloboplay/MainActivity.kt
+++ app/src/main/java/dev/paulowos/desafiogloboplay/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import dev.paulowos.desafiogloboplay.ui.screen.HomeScreen
 import dev.paulowos.desafiogloboplay.ui.theme.DesafioGloboplayTheme
+import dev.paulowos.desafiogloboplay.util.Routes
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -17,9 +21,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DesafioGloboplayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.HOME_SCREEN,
+                    modifier = Modifier.safeContentPadding()
+                ) {
+                    composable(Routes.HOME_SCREEN) {
+                        HomeScreen(navController)
+                    }
+
                 }
             }
+
         }
     }
 }
---
+++ app/src/main/java/dev/paulowos/desafiogloboplay/util/Routes.kt
@@ -0,0 +1,5 @@
+package dev.paulowos.desafiogloboplay.util
+
+object Routes {
+    const val HOME_SCREEN = "home"
+}
